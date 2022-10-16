79fe1f1745a9bcffd4d35c1d01900a89765da42a
dev-fa <franco.a.dev@proton.me>
2022-10-16 16:11:49 +0800

    Complete game flow
    Completed general flow of the game. Will display winner based on the
    board. Will display tie message when no one wins. Reset game board
    at game end. Add play again button once game ends.

--- script.js
+++ script.js
@@ -8,103 +8,130 @@ const gameBoard = (function () {
 
     const updateBoard = (box, symbol) => {
         boardArr.splice(box, 1, symbol);
-        checkWin(symbol);
+        let status = checkWin(symbol);
+        game.end(status);
+    };
+
+    const resetBoard = () => {
+        boardArr.splice(0, 9,
+            "", "", "", 
+            "", "", "", 
+            "", "", "",)
     };
 
     /* 3 IN A ROW
-
-    SIDES
+    sides
     i0 + i1 + i2
     i0 + i3 + i6
     i6 + i7 + i8
     i2 + i5 + i8
-    CENTER
+    center
     i1 + i4 + i7
     i3 + i4 + i5
-    DIAGONAL
+    diagonal
     i0 + i4 + i8
-    i2 + i4 + i6
-    */
+    i2 + i4 + i6 */
 
     const checkWin = (symbol) => {
+        let win;
         if (boardArr[0] === symbol && boardArr[1] === symbol &&
             boardArr[2] === symbol) {
 
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
 
         } else if (boardArr[0] === symbol && boardArr[3] === symbol &&
             boardArr[6] === symbol) {
             
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
 
         } else if (boardArr[6] === symbol && boardArr[7] === symbol &&
             boardArr[8] === symbol) {
             
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
             
         } else if (boardArr[2] === symbol && boardArr[5] === symbol &&
             boardArr[8] === symbol) {
             
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
             
         } else if (boardArr[1] === symbol && boardArr[4] === symbol &&
             boardArr[7] === symbol) {
             
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
             
         } else if (boardArr[3] === symbol && boardArr[4] === symbol &&
             boardArr[5] === symbol) {
             
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
             
         } else if (boardArr[0] === symbol && boardArr[4] === symbol &&
             boardArr[8] === symbol) {
             
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
             
         } else if (boardArr[2] === symbol && boardArr[4] === symbol &&
             boardArr[6] === symbol) {
             
             if (symbol === "X") {
-                console.log("player 1 wins");
+                displayController.displayWinner("PLAYER 1 WINS!");
+                return win = true;
             } else if (symbol === "O") {
-                console.log("player 2 wins");
+                displayController.displayWinner("PLAYER 2 WINS!");
+                return win = true;
             }
             
+        } else if (!(boardArr.includes(""))) {
+            displayController.displayWinner("NO ONE WINS");
+            return win = true;
         }
         
     };
 
     return {
         updateBoard,
+        resetBoard,
         boardArr
     };
 
@@ -121,7 +148,7 @@ const displayController = (function() {
         }
 
         for (let box in board) {
-            let boxElement = document.createElement("div");
+            let boxElement = document.createElement("button");
             boxElement.classList.add("box");
             boxElement.setAttribute("id", `box-${box}`);
             boxElement.addEventListener("click", addSymbol)
@@ -132,9 +159,9 @@ const displayController = (function() {
     const displayPlayer = () => {
         let currentPlayer;
         if (game.currentSymbol[0] === "X") {
-            currentPlayer = "Player 1";
+            currentPlayer = "(X) Player 1";
         } else {
-            currentPlayer = "Player 2";
+            currentPlayer = "(O) Player 2";
         }
 
         const infoContainer = document.querySelector(".game-info");
@@ -143,8 +170,38 @@ const displayController = (function() {
         }
         let playerStatus = document.createElement("p");
         playerStatus.classList.add("player-status");
-        playerStatus.textContent = `It is ${currentPlayer}'s turn!`;
+        playerStatus.textContent = `It is ${currentPlayer}'s turn`;
         infoContainer.appendChild(playerStatus);
+    };
+
+    const removeDisplayPlayer = () => {
+        const resetButton = document.createElement("button");
+        resetButton.classList.add("btn", "btn-primary");
+        resetButton.setAttribute("id", "play-again");
+        resetButton.textContent = "Play Again";
+        resetButton.addEventListener("click", game.play);
+
+        const infoContainer = document.querySelector(".game-info");
+        while (infoContainer.firstChild) {
+            infoContainer.removeChild(infoContainer.lastChild);
+        }
+        infoContainer.appendChild(resetButton);
+    }
+
+    const displayWinner = (string) => {
+        const winner = document.createElement("p");
+        winner.classList.add("winner-status");
+        winner.textContent = string;
+
+        const announceContainer = document.querySelector(".game-announcement");
+        announceContainer.appendChild(winner);
+    };
+
+    const removeDisplayWinner = () => {
+        const announceContainer = document.querySelector(".game-announcement");
+        while (announceContainer.firstChild) {
+            announceContainer.removeChild(announceContainer.lastChild);
+        }
     };
 
     const addSymbol = function (e) {
@@ -156,14 +213,18 @@ const displayController = (function() {
             e.target.textContent = playerSymbol;
             let boxIndex = e.target.id;
             boxIndex = boxIndex.substr(boxIndex.length-1);
-            gameBoard.updateBoard(boxIndex, playerSymbol);
+
             game.updateTurn();
             displayPlayer();
+            gameBoard.updateBoard(boxIndex, playerSymbol);
         }
     };
 
     return {
-        displayBoard
+        displayBoard,
+        removeDisplayPlayer,
+        displayWinner,
+        removeDisplayWinner
     };
 
 })();
@@ -183,6 +244,7 @@ const game = (function () {
     let currentSymbol = ["X"];
 
     const play = () => {
+        displayController.removeDisplayWinner();
         displayController.displayBoard(gameBoard.boardArr);
     };
 
@@ -194,9 +256,22 @@ const game = (function () {
         }
     };
 
+    const end = (winStatus) => {
+        if (winStatus) {
+            const boxes = document.getElementsByClassName("box");
+            for (let box in boxes) {
+                boxes[box].disabled = true;
+            }
+
+            displayController.removeDisplayPlayer();
+            gameBoard.resetBoard();
+        };
+    };
+
     return {
         play,
         updateTurn,
+        end,
         currentSymbol
     }
 
@@ -204,3 +279,4 @@ const game = (function () {
 
 
 document.getElementById("play").addEventListener("click", game.play);
+console.log("Created by https://github.com/dev-fa")
